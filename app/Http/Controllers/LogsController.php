2c71cabbaedb32715f95188870de989926044465
andresayac <andresayac@gmail.com>
2024-06-23 19:49:59 -0500

    refactor: Remove unused Hana helper class and related code

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -2,14 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Helpers\Hana;
 use App\Models\DocumentsLog;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Crypt;
-use Carbon\Carbon;
-use App\Traits\SapApi;
-use Illuminate\Support\Facades\Auth;
 
 class LogsController extends Controller
 {
