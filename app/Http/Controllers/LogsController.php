54bab3b00a846309e01c99a28dcb2af901fb5023
andresayac <andresayac@gmail.com>
2024-06-24 19:49:31 -0500

    refactor: Add admin check to LogsController methods

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -8,12 +8,22 @@ class LogsController extends Controller
 {
     public function index()
     {
+        // check if user is admin
+        if (!auth()->user()->is_admin) {
+            return redirect()->route('inicio');
+        }
+
         $documents_log = DocumentsLog::orderBy('id', 'desc')->paginate(10);;
         return view('logs.index', compact('documents_log'));
     }
 
     public function details(DocumentsLog $log)
     {
+        // check if user is admin
+        if (!auth()->user()->is_admin) {
+            return redirect()->route('inicio');
+        }
+
         return view('logs.details', compact('log'));
     }
 }
